ec09ef7fc78593496e678e428f93ad6ce83e26a2
supasuma <supasuma@gmail.com>
2016-09-22 12:24:13 +0100

    closes #25 user session stored

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,29 +1,29 @@
 var models = require('../models');
 var express = require('express');
 var router = express.Router();
+var current_user;
+var user;
 
 /* GET users listing. */
 router.get('/new', function(req, res, next) {
   res.render('sign_up', {
     title: "User sign up"
   });
-  // res.send('respond with a resource');
 });
 
 router.post('/', function(req, res, next) {
-  models.User.create({ Name: req.body.name,
+  sess = req.session;
+  user = models.User.create({ Name: req.body.name,
                        email: req.body.email,
                        password: req.body.password,
                        password_confirmation: req.body.password_confirmation})
   .then(function() {
+    sess.current_user = user._boundTo.dataValues.id;
     res.redirect('/');
   })
   .catch(function(error) {
     res.redirect('/users/new');
   });
 });
-
-// router.get('/users/new', function(req, res, next) {
-// });
 
 module.exports = router;
